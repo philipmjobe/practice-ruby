a8b5c3b1cb0109cc4dbf4d69c4de355b91c5fd00
Philip <philipmjobe@gmail.com>
2021-10-06 14:11:55 -0400

    added input option hash

--- store-item.rb
+++ store-item.rb
@@ -2,17 +2,17 @@ class Store_Items
   attr_reader :price, :color, :shape, :name
   attr_writer :color
 
-  def initialize(price, color, shape, name)
-    @price = price
-    @color = color
-    @shape = shape
-    @name = name
+  def initialize(input_option)
+    @price = input_option[:price]
+    @color = input_option[:color]
+    @shape = input_option[:shape]
+    @name = input_option[:name]
   end
 end
 
-item_1 = Store_Items.new(15, "orange", "round", "basketball")
-item_2 = Store_Items.new(20, "brown", "oval", "football")
-item_3 = Store_Items.new(5, "black", "disk", "hockey puck")
+item_1 = Store_Items.new(price: 15, color: "orange", shape: "round", name: "basketball")
+item_2 = Store_Items.new(price: 20, color: "brown", shape: "oval", name: "football")
+item_3 = Store_Items.new(price: 5, color: "black", shape: "disk", name: "hockey puck")
 
 puts item_3.price
 puts item_3.color
