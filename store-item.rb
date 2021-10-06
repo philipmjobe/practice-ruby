f981eb017ca2fc0c20954eb94b7aa0318a02748a
Philip <philipmjobe@gmail.com>
2021-10-06 13:46:29 -0400

    adds items to store

---
+++ store-item.rb
@@ -0,0 +1,19 @@
+class Store_Items
+  attr_reader :price, :color, :shape, :name
+
+  def initialize(price, color, shape, name)
+    @price = price
+    @color = color
+    @shape = shape
+    @name = name
+  end
+end
+
+item_1 = Store_Items.new(15, "orange", "round", "basketball")
+item_2 = Store_Items.new(20, "brown", "oval", "football")
+item_3 = Store_Items.new(5, "black", "disk", "hockey puck")
+
+puts item_3.price
+puts item_3.color
+puts item_3.shape
+puts item_3.name
