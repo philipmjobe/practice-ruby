46d4fd1a00168a31b7b029b44018773e42bae581
Philip <philipmjobe@gmail.com>
2021-10-07 14:54:02 -0400

    added more exercises

--- excercises.rb
+++ excercises.rb
@@ -1,1 +1,6 @@
+recipes = ["Pizza", "Pasta", "Mac and Cheese", "q", "w", "e", "r", "t", "y", "u", "crepes"]
+languages = ["q", "w", "e", "r", "French"]
 
+if recipes.include?("crepes") || languages.include?("French")
+  p "Sam and Sally get married"
+end
